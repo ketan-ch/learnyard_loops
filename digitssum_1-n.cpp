6fdc3caf85518afa3a43682abd2dba0aa4573f02
ketan-ch <163719646+ketan-ch@users.noreply.github.com>
2024-03-17 15:10:06 +0530

    Create digitssum_1-n.cpp

---
+++ digitssum_1-n.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n,sum=0;
+    cin>>n;
+    for (int i=1;i<=n;i++)
+    {
+        for(int rem=i;rem>0;rem=rem/10)
+        {
+            sum=sum+(rem%10);
+        }
+        cout<<sum<<endl;
+        sum=0;
+    }
+    
+    return 0;
+    
+}
